ff4e13e2f80155b554152489ce853b7a7588476e
tonyz74 <73653713+tonyz74@users.noreply.github.com>
2023-06-09 12:46:24 +0800

    Live updating of a view showing the "sentence"

--- cmd_auto.py
+++ cmd_auto.py
@@ -4,6 +4,7 @@ from cmd_common import CommandTabCommon
 from button import Button
 from input import Inputs
 from input_dialog import InputDialog
+import word
 
 
 class CommandTabAutomatic(CommandTabCommon):
@@ -84,6 +85,50 @@ class CommandTabAutomatic(CommandTabCommon):
 
         for b in self.timeline_buttons:
             b.render(self.screen)
+
+        pg.draw.rect(self.screen, c.WHITE, pg.Rect(
+            (20 - 1, 240 - 1),
+            (640 + 2, 160 + 3)
+        ))
+
+        preview_pos = [20, 240]
+
+        for row in range(c.TIMELINE_N_ROWS):
+            preview_pos[0] = 20
+
+            for col in range(int(c.TIMELINE_ROW_N_SLOTS / 2)):
+                # split by on & off
+                n = row * c.TIMELINE_ROW_N_SLOTS + col * 2
+
+                pg.draw.rect(self.screen, c.LIGHT_GRAY, pg.Rect(
+                    (preview_pos[0], preview_pos[1]),
+                    (150, 80)
+                ))
+
+                if word.word_list.word_exists(self.timeline_data[n]):
+                    w = word.word_list.find_word(self.timeline_data[n])
+                    for (off, enabled) in enumerate(w.vibs):
+                        if enabled:
+                            pg.draw.rect(self.screen, c.RED, pg.Rect(
+                                (preview_pos[0] + 15 * off, preview_pos[1]),
+                                (15, 80)
+                            ))
+
+                pg.draw.rect(self.screen, c.BLACK, pg.Rect(
+                    (preview_pos[0] + 150, preview_pos[1]),
+                    (10, 80)
+                ))
+
+                preview_pos[0] += 160
+            preview_pos[1] += 80 + 1
+
+        ln_start = [20, 240]
+        for col in range(int(c.TIMELINE_ROW_N_SLOTS / 2)):
+            x = ln_start[0] + 160 * col
+            for subcol in range(10):
+                x += 15
+                pg.draw.line(self.screen, c.WHITE, (x, ln_start[1]), (x, ln_start[1] + 161))
+
 
         return surf
 
